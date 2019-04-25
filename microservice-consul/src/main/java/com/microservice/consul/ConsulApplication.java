100f940f567587a15ac95439b341cbbc07c0baf1
Neon Too <469565714@qq.com>
2019-04-25 09:49:41 +0800

    add DiscoveryClient and update yml

--- microservice-consul/src/main/java/com/microservice/consul/ConsulApplication.java
+++ microservice-consul/src/main/java/com/microservice/consul/ConsulApplication.java
@@ -1,12 +1,17 @@
 package com.microservice.consul;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ConsulApplication {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     public static void main(String[] args) {
         SpringApplication.run(ConsulApplication.class, args);
