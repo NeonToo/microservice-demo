c81d23220bc5ad03e32e4cbf74a8b822a4ced744
NeonToo <469565714@qq.com>
2019-04-26 17:13:27 +0800

    fix port bug for FrontendApplication and add new dependency

--- microservice-zipkin/src/main/java/com/tencent/demo/zipkin/FrontendApplication.java
+++ microservice-zipkin/src/main/java/com/tencent/demo/zipkin/FrontendApplication.java
@@ -11,16 +11,17 @@ import org.springframework.web.client.RestTemplate;
 
 @EnableAutoConfiguration
 @RestController
-@CrossOrigin
+@CrossOrigin  // for cross-origin JS
 public class FrontendApplication {
 
-    private static final String BASE_URL = System.getProperty("spring.baseUrl", "http://localhost:9000");
+    private static final String BASE_URL = "http://localhost:9000";
 
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/")
     public String callBackend() {
+        System.out.print(BASE_URL + "/api");
         return restTemplate.getForObject(BASE_URL + "/api", String.class);
     }
 
@@ -30,7 +31,7 @@ public class FrontendApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(FrontendApplication.class, "--spring.application.name=java-frontend", "--server-port=8081");
+        SpringApplication.run(FrontendApplication.class, "--spring.application.name=java-frontend");
     }
 
 }
