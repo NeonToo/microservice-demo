dd49d7147659d0b5e8ae2602540b2314ef1ca251
NeonToo <469565714@qq.com>
2019-05-09 17:29:56 +0800

    add logger for

--- microservice-zipkin/src/main/java/com/tencent/demo/zipkin/web/WebConfig.java
+++ microservice-zipkin/src/main/java/com/tencent/demo/zipkin/web/WebConfig.java
@@ -1,6 +1,8 @@
 package com.tencent.demo.zipkin.web;
 
 import com.tencent.demo.zipkin.web.interceptor.CustomizedHandlerInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.CollectionUtils;
@@ -15,17 +17,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired(required = false)
     protected List<CustomizedHandlerInterceptor> customizedHandlerInterceptors;
 
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+
+        logger.debug(" *** Add Customized Interceptors ***");
+
         if(!CollectionUtils.isEmpty(customizedHandlerInterceptors)) {
             for(final CustomizedHandlerInterceptor customizedHandlerInterceptor: customizedHandlerInterceptors) {
                 registry.addInterceptor(customizedHandlerInterceptor);
             }
         }
-
-//        super.addInterceptors(registry);
     }
-
-
 
 }
